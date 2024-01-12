5abbffe5c14f045d945e0318547fc79ec2bd87c4
Peluri Ramya <119730513+22p31a0562@users.noreply.github.com>
2024-01-12 14:44:59 +0530

    import java.util.Scanner; public class Pro{     public static void main(String[] args)     {         Scanner s=new Scanner(System.in);         double a=s.nextDouble();         double res=(a)*5/18;         System.out.printf("%.2f",res);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Pro{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        double a=s.nextDouble();
+        double res=(a)*5/18;
+        System.out.printf("%.2f",res);
+    }
+}
