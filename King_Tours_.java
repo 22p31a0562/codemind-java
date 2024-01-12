be3002ca9c50e277e911be2a7844a7498d4473c0
Peluri Ramya <119730513+22p31a0562@users.noreply.github.com>
2024-01-12 13:41:25 +0530

    import java.util.Scanner; public class Pro{     public static void main(String[] args)     {         Scanner s=new Scanner(System.in);         int a=s.nextInt();         int b=s.nextInt();         System.out.print(a*5+b*7);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Pro{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        System.out.print(a*5+b*7);
+    }
+}
