0a0c91e7a08dc95ad05002663522f119d485767e
Peluri Ramya <119730513+22p31a0562@users.noreply.github.com>
2024-01-13 11:47:48 +0530

    import java.util.Scanner; public class Pro{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(n>=18)         {             System.out.print("YES");         }         else         {             System.out.print("NO");         }     } }

---
+++ Casting_the_Vote.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Pro{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>=18)
+        {
+            System.out.print("YES");
+        }
+        else
+        {
+            System.out.print("NO");
+        }
+    }
+}
