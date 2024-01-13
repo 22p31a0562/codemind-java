9ea5f2f30969c5853416b01281ff8ccd01e61da1
Peluri Ramya <119730513+22p31a0562@users.noreply.github.com>
2024-01-13 11:59:03 +0530

    import java.util.Scanner; public class Pro{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(n<3){             System.out.print("LIGHT");         }         else if(n>=3 && n<7)         {             System.out.print("MODERATE");         }         else         {             System.out.print("HEAVY");         }     } }

---
+++ Rain_Fall.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Pro{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n<3){
+            System.out.print("LIGHT");
+        }
+        else if(n>=3 && n<7)
+        {
+            System.out.print("MODERATE");
+        }
+        else
+        {
+            System.out.print("HEAVY");
+        }
+    }
+}
