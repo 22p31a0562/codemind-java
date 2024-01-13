5c30fdf1cf147bd54b9971e972082030f74cdf2b
Peluri Ramya <119730513+22p31a0562@users.noreply.github.com>
2024-01-13 11:55:45 +0530

    import java.util.Scanner; public class Pro{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(n>98)         {             System.out.print("YES");         }         else         {             System.out.print("NO");         }     } }

---
+++ Fever.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Pro{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>98)
+        {
+            System.out.print("YES");
+        }
+        else
+        {
+            System.out.print("NO");
+        }
+    }
+}
