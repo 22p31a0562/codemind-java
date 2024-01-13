306d763b784d30fddc3478f150cd450ecf0e0d32
Peluri Ramya <119730513+22p31a0562@users.noreply.github.com>
2024-01-13 12:04:30 +0530

    import java.util.Scanner; public class Pro{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int r=y-x;         System.out.print(r);     } }

---
+++ Donation.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Pro{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int r=y-x;
+        System.out.print(r);
+    }
+}
